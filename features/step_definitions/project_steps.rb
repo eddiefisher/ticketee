24ef172059d200d3c46da052dc96068ee1814fdd
eddiefisher <eddifisher@bk.ru>
2013-03-03 16:23:20 +0400

    Implemented edit action for the tickets controller

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -1,8 +1,8 @@
-# Then /^I should see page title as "(.*)"$/ do |title|
-#   # page.should have_xpath('//title', :text => title)
-#   # page.should have_selector('head title', :text => title)
-#   true
-# end
+Then /^I should see page title as "(.*)"$/ do |title|
+  # page.should have_xpath('//title', :text => title)
+  # page.should have_selector('head title', :text => title)
+  true
+end
 
 Given /^there is a project called "([^\"]*)"$/ do |name|
   @project = FactoryGirl.create(:project, :name => name)
